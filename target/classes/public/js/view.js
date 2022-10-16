48ee07c434ff0eb4ab89bfa264802adca9682a95
anandgidz <67944285+anandgidz@users.noreply.github.com>
2022-10-16 15:34:22 -0500

    Tabs initial

--- target/classes/public/js/view.js
+++ target/classes/public/js/view.js
@@ -1,6 +1,12 @@
 'use strict';
 
 window.onload = function() {
+    $('#form-login').submit(function(e) {
+        e.preventDefault();
+        console.log("hello");
+        window.location.href = "/main";
+    })
+
     $('#form-signup').submit(function(e) {
         e.preventDefault();
         console.log("hello");
