83ec391e7bc293212ccde3ec1ff3f7029fbb6dbe
anandgidz <67944285+anandgidz@users.noreply.github.com>
2022-10-17 21:37:49 -0500

    improvements to main gui

--- target/classes/public/js/view.js
+++ target/classes/public/js/view.js
@@ -4,12 +4,11 @@ window.onload = function() {
     $('#form-login').submit(function(e) {
         e.preventDefault();
         console.log("hello");
-        window.location.href = "/main";
+        window.location.href = "/main.html";
     })
 
     $('#form-signup').submit(function(e) {
         e.preventDefault();
-        console.log("hello");
-        window.location.href = "/main";
+        window.location.href = "/main.html";
     })
 };
